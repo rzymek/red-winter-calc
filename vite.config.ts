c1eab0e2b383b4b4098e69bf41eb83a0592bc994
Krzysztof Rzymkowski <rzymek@gmail.com>
2023-12-16 14:26:04 +0100

    vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,43 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import preact from '@preact/preset-vite'
+import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/tcs-calc',
-  plugins: [preact()],
+    base: '/tcs-calc',
+    plugins: [
+        preact(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: "TCS Calculator",
+                short_name: 'tcs-calc',
+                description: "TCS Calculator",
+                theme_color: "#4e9a06",
+                "icons": [
+                    {
+                        "src": "pwa-64x64.png",
+                        "sizes": "64x64",
+                        "type": "image/png"
+                    },
+                    {
+                        "src": "pwa-192x192.png",
+                        "sizes": "192x192",
+                        "type": "image/png"
+                    },
+                    {
+                        "src": "pwa-512x512.png",
+                        "sizes": "512x512",
+                        "type": "image/png"
+                    },
+                    {
+                        "src": "maskable-icon-512x512.png",
+                        "sizes": "512x512",
+                        "type": "image/png",
+                        "purpose": "maskable"
+                    }
+                ]
+            }
+        })
+    ],
 })
