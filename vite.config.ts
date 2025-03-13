aa67eec3dd0e5039fe860f4f1903e1f9ed0d9f8f
rzymek <krzysztof.rzymkowski@gmail.com>
2025-03-13 20:59:58 +0100

    pwa improvements

--- vite.config.ts
+++ vite.config.ts
@@ -15,26 +15,34 @@ export default defineConfig({
             registerType: 'autoUpdate',
             manifest: {
                 name: "Red Winter",
+                id: 'redwinter',
                 short_name: 'redwinter',
+                orientation: "portrait",
+                launch_handler: {
+                    client_mode: "auto"
+                },
                 description: "Red Winter",
                 theme_color: "#FFFFE0",
                 icons: [{
                     src: "pwa-64x64.png",
                     sizes: "64x64",
-                    type: "image/png"
+                    type: "image/png",
+                    purpose: "any",
                 }, {
                     src: "pwa-192x192.png",
                     sizes: "192x192",
-                    type: "image/png"
+                    type: "image/png",
+                    purpose: "any",
                 }, {
                     src: "pwa-512x512.png",
                     sizes: "512x512",
-                    type: "image/png"
+                    type: "image/png",
+                    purpose: "any",
                 }, {
                     src: "maskable-icon-512x512.png",
                     sizes: "512x512",
                     type: "image/png",
-                    purpose: "maskable"
+                    purpose: "any maskable",
                 }]
             }
         })
