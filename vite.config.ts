2e992b3e4182a64c4f011d904099dd52d96d1c5b
rzymek <krzysztof.rzymkowski@gmail.com>
2025-03-13 19:12:36 +0100

    base: '/red-winter-calc',

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/redwinter',
+    base: '/red-winter-calc',
     test: {
         environment: "jsdom",
     },
