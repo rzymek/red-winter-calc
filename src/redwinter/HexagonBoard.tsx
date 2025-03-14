c970a765ba1c56fd6b1dfdfbdfb9be6c7772901c
rzymek <krzysztof.rzymkowski@gmail.com>
2025-03-14 21:12:55 +0100

    refactor

--- src/redwinter/HexagonBoard.tsx
+++ src/redwinter/HexagonBoard.tsx
@@ -107,20 +107,22 @@ export function HexagonBoard() {
 }
 
 function Bridge() {
+    const borderWidth = 12;
+    const left = '3 95 20 63 127 63 143 95';
+    const right = '3 2 20 34 127 34 143 2';
+    const fill = 'none';
     return (
-        <g transform="matrix(0,0.42,-0.42,0,293,509) translate(-1026,291)" style="pointer-events: none;">
-            <polyline
-                style="fill:none;stroke:#fff;stroke-width:12"
-                points="3 95 20 63 127 63 143 95"/>
-            <polyline
-                style="fill:none;stroke:#fff;stroke-width:12"
-                points="3 2 20 34 127 34 143 2"/>
-            <polyline
-                style="fill:none;stroke:#000;stroke-width:8"
-                points="3 95 20 63 127 63 143 95"/>
-            <polyline
-                style="fill:none;stroke:#000;stroke-width:8"
-                points="3 2 20 34 127 34 143 2"/>
+        <g transform="matrix(0,0.42,-0.42,0,293,509) translate(-1026,291)"
+           style={{pointerEvents: 'none'}}>
+            <polyline points={left}
+                      style={{fill, stroke: '#fff', strokeWidth: borderWidth}}/>
+            <polyline points={left}
+                      style={{fill, stroke: '#000', strokeWidth: borderWidth - 4}}/>
+
+            <polyline points={right}
+                      style={{fill, stroke: '#fff', strokeWidth: borderWidth}}/>
+            <polyline points={right}
+                      style={{fill, stroke: '#000', strokeWidth: borderWidth - 4}}/>
         </g>
     );
 }
