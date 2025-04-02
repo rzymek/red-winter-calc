f9a5bb1b3c05b634042105f966cb62256b72dbc2
rzymek <krzysztof.rzymkowski@gmail.com>
2025-04-03 00:41:27 +0200

    test

--- src/redwinter/calc/crt.spec.tsx
+++ src/redwinter/calc/crt.spec.tsx
@@ -1,10 +1,18 @@
 import {describe, expect, it} from "vitest";
 import {combatResult} from "./crt.ts";
 
+function result(r: string) {
+    const [attacker, defender ] = r.split('/').map(it=>Number(it));
+    return {
+        attacker,
+        defender
+    }
+}
+
 describe('crt', () => {
-    it('should', () => {
-        expect(combatResult(12,'6:1')).toEqual('0/8');
-        expect(combatResult(7,'1:1')).toEqual('1/0');
-        expect(combatResult(2,'1:4')).toEqual('5/0');
-    })
+    it('should return correct combat results for various dice rolls and ratios', () => {
+        expect(combatResult(12, '6:1')).toEqual(result('0/8'));
+        expect(combatResult(7, '1:1')).toEqual(result('1/0'));
+        expect(combatResult(2, '1:4')).toEqual(result('5/0'));
+    });
 });
